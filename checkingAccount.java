0dc136988783f51b36a75b83173665699ae799bb
Chelsearspencer <71734078+Chelsearspencer@users.noreply.github.com>
2021-11-08 00:50:46 -0500

    checking

---
+++ checkingAccount.java
@@ -0,0 +1,23 @@
+
+public class checkingAccount extends bankAccountClass{
+
+public checkingAccount(String name, double accountNumber, double balance) {
+		super(name, accountNumber, balance);
+
+}
+
+void MonthEnd(double averageDailyBalance) {
+
+	   
+	   	if(getBalance()< 1500) {
+		{
+			Withdraw(20);
+		}
+}
+}
+public String toString() {
+		String s=super.toString();
+		return s;
+
+}}
+
